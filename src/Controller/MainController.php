417523c4ca7568356670c7dee7e7f3b5a52c48b4
DeuklyoungKo <deutschgon@gmail.com>
2018-10-17 05:20:02 +0000

    setting basic design

---
+++ src/Controller/MainController.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 2018-10-15
+ * Time: 07:19
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+
+
+    /**
+     * @Route("/")
+     */
+    public function main()
+    {
+        return $this->render('main/main.html.twig',[
+
+        ]);
+    }
+
+
+    /**
+     * @Route("/info")
+     */
+    public function info()
+    {
+        return $this->render('info/info.html.twig',[
+
+        ]);
+    }
+
+
+    /**
+     * @Route("/get/{idx}")
+     */
+    public function getJoinInfo($idx)
+    {
+        return new Response('getJoinInfo todo');
+    }
+}
